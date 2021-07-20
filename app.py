649acf9947158efe670e11a805f4e26ae7fd34c2
Jake Peck <jakepeck44@gmail.com>
2021-07-20 09:21:25 -0500

    pre deploy on netlify

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ cors = CORS(app)
 api = Api(app)
 
 # Deployment configuration
-DATABASE_URL = os.getenv('DATABASE_URL', 'APP_SECRET')
+DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace(
